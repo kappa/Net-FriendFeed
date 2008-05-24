24ae6f70131e2e1976ea236d345f2f7bb26eeda8
Alex Kapranoff <alex@kapranoff.ru>
2008-05-25 00:21:26 +0400

    Make scripts more like examples.

--- scripts/share-pic.pl
+++ scripts/share-pic.pl
@@ -7,9 +7,11 @@ use utf8;
 
 use Net::FriendFeed;
 
-my $frf = new Net::FriendFeed { login => 'kkapp', remotekey => 'gibes12amend' };
-#my $frf = new Net::FriendFeed { login => 'kappak', remotekey => 'zoo550tubas' };
+my $frf = new Net::FriendFeed { login => 'kkapp', remotekey => 'remo500key' };
 
-$frf->publish_link(Encode::encode('utf-8', 'Последний в этом сезоне Хаус (seeds: 14462, leechers: 4304'),
+$frf->publish_link(
+    # en: Last House in this season (seeds: ...)'
+    'Последний в этом сезоне Хаус (seeds: 14462, leechers: 4304)',
     'http://isohunt.com/torrents/?ihq=house+s04e16',
-    ['http://de6.pictaboo.com/f/8/v69e83b88t74i69q83b89d72f72i69.jpg']);
+    ['http://de6.pictaboo.com/f/8/v69e83b88t74i69q83b89d72f72i69.jpg']
+);
--- scripts/share.pl
+++ scripts/share.pl
@@ -4,6 +4,6 @@ use warnings;
 
 use Net::FriendFeed;
 
-my $frf = new Net::FriendFeed { login => 'kappak', remotekey => 'zoo550tubas' };
+my $frf = new Net::FriendFeed { login => 'kkapp', remotekey => 'remo501key' };
 
 $frf->publish_message('Hello, world!');
